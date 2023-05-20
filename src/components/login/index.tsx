0e38e3a2dffd0a2023bd6bf6b0d52a1b89359cdf
Joel Flores <joelf.-@live.com>
2023-05-20 04:37:00 -0300

    Fix initialvalue

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -12,7 +12,6 @@ const Login: FunctionComponent<Props> = ({ onSubmit }) => {
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
         style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
         onFinish={onSubmit}
         autoComplete="off"
       >
--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -16,7 +16,6 @@ const Register: FunctionComponent<Props> = ({ onSubmit }) => {
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
         style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
         onFinish={onSubmitForm}
         autoComplete="off"
       >
